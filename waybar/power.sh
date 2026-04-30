e6ba049f50c1295c89b0e7b2d88912c66660cab8
tau-zeta <taufiqhaikal1808@gmail.com>
2026-04-30 14:11:49 +0800

    v2 - Added Power options script

---
+++ waybar/power.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+options="󰤄 Suspend\n󰜉 Restart\n󰐥 Power Off"
+
+chosen=$(echo -e "$options" | wofi --dmenu --width 300 --height 125 --cache-file /dev/null --prompt "Power Menu")
+
+case "$chosen" in
+    *Suspend)
+        systemctl suspend
+        ;;
+    *Restart)
+        systemctl reboot
+        ;;
+    *"Power Off")
+        systemctl poweroff
+        ;;
+    *)
+        exit 1
+        ;;
+esac
